72750c4843ffe41f9680c9a8e58fa45e0689736a
Manu <manuel@volt.agency>
2018-01-05 17:57:57 +0100

    Browsersync Funktionalität

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ postcss = require("gulp-postcss"),
 autoprefixer = require("autoprefixer"),
 cssvars = require("postcss-simple-vars"),
 nested = require("postcss-nested"),
-cssImport = require("postcss-import");
+cssImport = require("postcss-import"),
+browserSync = require("browser-sync").create();
 
 gulp.task('default', function() {
   console.log("Hooray - you created a Gulp task.");
@@ -22,12 +23,24 @@ gulp.task('styles', function() {
 
 gulp.task('watch', function() {
 
+  browserSync.init({
+    notify:false,
+    server: {
+      baseDir:"app"
+    }
+  });
+
   watch('./app/index.html', function() {
-    gulp.start('html');
+    browserSync.reload();
   });
 
   watch('./app/assets/styles/**/*.css', function() {
-    gulp.start('styles');
+    gulp.start('cssInject');
   });
 
+});
+
+gulp.task("cssInject", ["styles"], function() {
+  return gulp.src("./app/temp/styles/styles.css")
+    .pipe(browserSync.stream());
 });
