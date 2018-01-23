376baffd1d5bfd2e85126a080107aeec7ea63f9f
Manu <manuel@volt.agency>
2018-01-23 18:24:16 +0100

    Waypoints reveal items

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,5 @@
 import MobileMenu from "./modules/MobilMenu";
+import RevealOnScroll from "./modules/RevealOnScroll";
 
 var mobilMenu = new MobileMenu();
+var revealOnScroll = new RevealOnScroll();
