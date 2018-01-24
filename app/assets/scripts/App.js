5e11a748fac4954a3729000d7a653654b8f4291b
Manu <manuel@volt.agency>
2018-01-24 17:11:29 +0100

    Completed Sticky Header Menu

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import $ from "jquery";
 import MobileMenu from "./modules/MobilMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
+import StickyHeader from "./modules/StickyHeader";
 
 var mobilMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"),"85%");
 new RevealOnScroll($(".testimonial"),"60%");
+var stickyHeader = new StickyHeader();
