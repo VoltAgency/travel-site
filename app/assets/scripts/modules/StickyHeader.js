c3865f1091ddbf788798ec494f55eac33a4f4abb
Manu <manuel@volt.agency>
2018-03-08 12:46:53 +0100

    completed lazy loading of images

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -4,6 +4,7 @@ import smoothScroll from "jquery-smooth-scroll";
 
 class StickyHeader{
     constructor(){
+      this.lazyImages = $(".lazyload");
       this.siteHeader = $(".site-header");
       this.headerTriggerElement = $(".large-hero__title");
       this.createHeaderWaypoint();
@@ -11,8 +12,14 @@ class StickyHeader{
       this.headerLinks = $(".primary-nav a");
       this.createPageSectionWaypoints();
       this.addSmoothScrolling();
+      this.refreshWaypoints();
     }
 
+    refreshWaypoints(){
+      this.lazyImages.on('load', function(){
+        Waypoint.refreshAll();
+      });
+    }
     addSmoothScrolling(){
       this.headerLinks.smoothScroll();
     }
